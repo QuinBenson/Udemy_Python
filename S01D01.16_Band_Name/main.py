098db60e67a537534b9e8dc9878bdc8f2b1c3f4e
unknown <quin.benson@gmail.com>
2023-06-15 11:59:42 +0100

    Day 01 Complete

---
+++ S01D01.16_Band_Name/main.py
@@ -0,0 +1,15 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
+print ("Willkommen! Bienvenue! Welcome!")
+l_placename = input ("placename?\n")
+l_petname = input ("petname?\n")
+print ("Banned name: " + l_placename + " " + l_petname )
