d0f5599a7807499c7e0645fea891f30481a19633
QuinB <Quin.Benson@gmail.com>
2023-07-06 12:13:14 +0100

    oops... download from codingrooms for S10D10.105_calculator

---
+++ S10D10.105_calculator/main.py
@@ -0,0 +1,9 @@
+import art
+import Calculator_fns
+
+print (f"{art.logo}")
+
+print (f"{Calculator_fns.add(3.141259,10)}")
+print (f"{Calculator_fns.subtract(3.141259,10)}")
+print (f"{Calculator_fns.multiply(3.141259,10)}")
+print (f"{Calculator_fns.divide(3.141259,10)}")
